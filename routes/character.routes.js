c5cc2d5d1bf9880e8bdd043951eeb0d6e9ffc54a
Rafael <rafael.f.coelho@outlook.com>
2022-11-28 12:01:36 +0000

    All Character Routes

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const Character = require('../models/Character.model');
+const User = require('../models/User.model');
 
-//! SAVE CHARACTER
+//! SAVE CHARACTER (POST route)
 
-router.post('/save-character', async(req, res, next) => {
+router.post('/save-character', async (req, res, next) => {
 
     try {
 
@@ -13,8 +14,10 @@ router.post('/save-character', async(req, res, next) => {
         const newCharacter = await Character.create({
             firstName, lastName, gender, 
             level, ancestry, background, 
-            charClass, deity
+            charClass, deity, attributes
         });
+
+        res.status(201).json(newCharacter);
         
     } catch (err) {
         res.json(err);
@@ -22,3 +25,83 @@ router.post('/save-character', async(req, res, next) => {
         
     };
 });
+
+//! GET ALL CHARACTERS route
+
+router.get('/user/:id/characterlist', async (req, res, next) =>{
+
+    const userId = req.params.id;
+
+    try {
+        const allChars = await User.findById(userId).populate("characters");
+
+        res.status(200).json(allChars);
+        
+    } catch (err) {
+        next(err);
+    }
+});
+
+//! GET SINGLE CHARACTER route
+
+router.get('/character/:id', async (req, res, next) => {
+
+    const charId = req.params.id;
+
+    try {
+        const specificChar = await Character.findById(charId);
+        res.status(200).json(specificChar);
+        
+    } catch (err) {
+        next(err);
+    }
+});
+
+//! EDIT (put) SINGLE CHARACTER route
+
+router.put('character/:id', async(req, res, next) => {
+
+    try {
+
+        const {id} = req.params;
+        const charId = id;
+        const {
+            firstName, lastName, gender, 
+            level, ancestry, background, 
+            charClass, deity, attributes
+        } = req.body;
+
+        const updatedChar = await Character.findByIdAndUpdate(id, {
+            firstName, lastName, gender, 
+            level, ancestry, background, 
+            charClass, deity, attributes
+        }, {new: true});
+
+        res.status(200).json(updatedChar);
+        
+    } catch (err) {
+        next(err);
+    }
+});
+
+//! DELETE (delete) SINGLE CHARACTER route
+
+router.delete('/character/:id', async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const charId = id;
+
+        const deletedChar = await Character.findByIdAndDelete(charId);
+
+        //Checking for Last Names because of Syntax shenanigans
+        if(deletedChar.lastName) {
+            res.status(200).json({message: `The character with the id '${charId}' and name '${deletedChar.firstName} + ${deletedChar.lastName}' (${deletedChar.charClass}) was deleted successfully.`});
+        } else {
+            res.status(200).json({message: `The character with the id '${charId}' and name '${deletedChar.firstName}' (${deletedChar.charClass}) was deleted successfully.`});
+        }
+    } catch (err) {
+        next(err); 
+    };
+});
+
+module.exports = router;
