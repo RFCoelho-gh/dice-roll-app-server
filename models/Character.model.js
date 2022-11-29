39af26f239410ea7a5ef84008307997829020000
Rafael <rafael.f.coelho@outlook.com>
2022-11-29 12:27:33 +0000

    Preventive Commit: Nesting in Char Model

--- models/Character.model.js
+++ models/Character.model.js
@@ -37,7 +37,7 @@ const characterSchema = new Schema(
             type: String,
             default: "None",
         },
-        attributes: {
+        attributes:  {
             strength: {
                 type: Number,
                 default: 10
